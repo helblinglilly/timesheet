27a22ff237dd82d694f4013037b62cf5e5ec240e
Lilly Helbling <lilly@helbling.uk>
2024-12-02 18:49:53 +0000

    Improve logging

--- src/app/auth/login/SocialAuthMethods.tsx
+++ src/app/auth/login/SocialAuthMethods.tsx
@@ -1,14 +1,27 @@
 "use server";
 import type { PocketbaseAuthMethods } from "@/lib/pocketbase.types";
+import { log } from "@/utils/log";
 import { headers } from "next/headers";
 import React from "react";
 import OAuthMethod from "./OAuthMethods";
 
 export default async function SocialAuthMethods() {
-	const res = fetch(
+	const res = await fetch(
 		`${process.env.POCKETBASE_URL}/api/collections/users/auth-methods`,
-	);
-	const body = await (await res).json();
+	).catch(async (err) => {
+		await log({ message: err, isError: true });
+		throw err;
+	});
+
+	if (!res.ok) {
+		await log({
+			message: "Non-200 status code when fetching auth methods",
+			status: res.status,
+		});
+		throw res.status;
+	}
+
+	const body = await res.json();
 	const authMethods = body.authProviders as Array<PocketbaseAuthMethods>;
 
 	const headersList = await headers();
