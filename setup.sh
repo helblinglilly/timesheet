e11f58e8f6c5929c1517490ba6367bbd22ea716c
Lilly Helbling <lilly@helbling.uk>
2024-12-04 22:10:22 +0000

    Remove CF specific instruction

--- setup.sh
+++ setup.sh
@@ -44,37 +44,37 @@ EOL
     fi
 fi
 
-# Step 2: Add file validation step
-echo "Would you like to add validation that a runtime has been specified for new 'page.tsx' files? (y/n)"
-read -r ADD_FILE_CHECK
-if [[ "$ADD_FILE_CHECK" =~ ^[Yy]$ ]]; then
-    if step_already_added "The following files are missing the required line"; then
-        echo "File validation step is already present in the pre-push hook."
-    else
-        echo "Adding runtime validation for 'page.tsx' step to the pre-push hook..."
-        cat <<'EOL' >> "$PRE_PUSH_HOOK"
+# # Step 2: Add file validation step
+# echo "Would you like to add validation that a runtime has been specified for new 'page.tsx' files? (y/n)"
+# read -r ADD_FILE_CHECK
+# if [[ "$ADD_FILE_CHECK" =~ ^[Yy]$ ]]; then
+#     if step_already_added "The following files are missing the required line"; then
+#         echo "File validation step is already present in the pre-push hook."
+#     else
+#         echo "Adding runtime validation for 'page.tsx' step to the pre-push hook..."
+#         cat <<'EOL' >> "$PRE_PUSH_HOOK"
 
-echo "Checking 'page.tsx' files in /src/app..."
-MISSING_RUNTIME_FILES=()
-FILES_TO_CHECK=$(git diff --cached --name-only | grep -E '^src/app/.*/page\.tsx$')
+# echo "Checking 'page.tsx' files in /src/app..."
+# MISSING_RUNTIME_FILES=()
+# FILES_TO_CHECK=$(git diff --cached --name-only | grep -E '^src/app/.*/page\.tsx$')
 
-for FILE in $FILES_TO_CHECK; do
-    if ! grep -q 'export const runtime = "edge";' "$FILE"; then
-        MISSING_RUNTIME_FILES+=("$FILE")
-    fi
-done
+# for FILE in $FILES_TO_CHECK; do
+#     if ! grep -q 'export const runtime = "edge";' "$FILE"; then
+#         MISSING_RUNTIME_FILES+=("$FILE")
+#     fi
+# done
 
-if [ ${#MISSING_RUNTIME_FILES[@]} -ne 0 ]; then
-    echo "The following files do not specify a runtime. This will cause the build to fail on cloudflare 'export const runtime = \"edge\"':"
-    for FILE in "${MISSING_RUNTIME_FILES[@]}"; do
-        echo " - $FILE"
-    done
-    echo "Please specify the runtime in the page.tsx files before pushing."
-    exit 1
-fi
-echo "Runtime specified in all new files!"
-EOL
-    fi
-fi
+# if [ ${#MISSING_RUNTIME_FILES[@]} -ne 0 ]; then
+#     echo "The following files do not specify a runtime. This will cause the build to fail on cloudflare 'export const runtime = \"edge\"':"
+#     for FILE in "${MISSING_RUNTIME_FILES[@]}"; do
+#         echo " - $FILE"
+#     done
+#     echo "Please specify the runtime in the page.tsx files before pushing."
+#     exit 1
+# fi
+# echo "Runtime specified in all new files!"
+# EOL
+#     fi
+# fi
 
 echo "Setup complete. Your pre-push hook has been updated."
