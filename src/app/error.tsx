f8ce1127aa8a35a822f98807e41772033659adaa
Lilly Helbling <lilly.helbling@onthebeach.co.uk>
2025-09-19 17:47:57 +0100

    Add the most basic error page

---
+++ src/app/error.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <pre>{error.message}</pre>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
