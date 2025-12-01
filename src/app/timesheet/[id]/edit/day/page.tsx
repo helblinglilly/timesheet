e385634486e0d6320953875b0af624eefb637693
Lilly Helbling <lilly@helbling.uk>
2025-12-01 23:25:33 +0000

    Fix width

--- src/app/timesheet/[id]/edit/day/page.tsx
+++ src/app/timesheet/[id]/edit/day/page.tsx
@@ -14,7 +14,7 @@ export default async function TimesheetEditPage(
   const { id } = await params;
 
   return (
-    <div className="md:grid px-4 pt-4 pb-8 justify-center w-full grid gap-8">
+    <div className="grid px-4 pt-4 pb-8 gap-8 w-full md:w-[90%] md:mx-auto lg:w-[80%] xl:w-[70%]">
       <EditBreadcrumbs id={id} />
 
       <EditTimesheetDay timesheetId={id} />
