8a57f0a608876d500ca90e4eb20daa9266d16f99
Lilly Helbling <lilly.helbling@onthebeach.co.uk>
2025-12-01 18:24:23 +0000

    Adjust meta content

--- src/app/timesheet/[id]/layout.tsx
+++ src/app/timesheet/[id]/layout.tsx
@@ -3,6 +3,23 @@ import type { TimesheetConfig } from '~/pocketbase/data.types';
 import { serverSideAuth } from '~/pocketbase/server';
 import { TableNames } from '~/pocketbase/tables.types';
 import { QueryParamInvalidator } from './QueryParamInvalidator';
+import type { Metadata } from 'next';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+  const pb = await serverSideAuth();
+  const { id } = await params;
+
+  const config = await pb.collection<TimesheetConfig>(TableNames.TimesheetConfig).getOne(id);
+
+  return {
+    title: [config.name, 'Timesheet'].filter(Boolean).join(' - '),
+    description: null
+  };
+}
 
 export default async function TimesheetIdLayout({
   children,
