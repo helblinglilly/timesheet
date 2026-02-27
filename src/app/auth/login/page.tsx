412072993d739b9a03e7cf333f65bcf67be7ba55
Lilly Helbling <lilly.helbling@onthebeach.co.uk>
2026-02-27 16:54:32 +0000

    Improve error boundary on login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,10 +5,58 @@ import { Navbar } from '~/features/Navbar/Navbar';
 import { AuthMethodsList } from '~/features/Authentication/OAuthProviders';
 import { Email } from '~/features/Authentication/Email';
 import { getAuthMethods } from '~/features/Authentication/getAuthMethods';
+import { Suspense } from 'react';
+
+async function AuthMethods() {
+  const { t } = await createTranslation();
+
+  try {
+    const authMethods = await getAuthMethods();
+    return (
+      <div className='grid gap-4'>
+        <AuthMethodsList authMethods={authMethods} />
+
+        <div className="grid gap-2">
+          <p>{ t('authentication.login.email.section_title') }</p>
+        </div>
+        <Email />
+      </div>
+    );
+
+  } catch (err) {
+    const fullError = new Error('/auth/login Failed to fetch auth methods on login page', {
+      cause: err
+    })
+    fullError.name = 'AuthMethodFetchFail';
+
+    // eslint-disable-next-line no-console
+    console.error(fullError);
+
+    return (
+      <div className='bg-red-200 p-4 mt-8 px-8 grid gap-2 rounded-sm md:max-w-2/3 mx-auto'>
+        <div className='pb-16'>
+          <h2 className='text-xl font-bold '>{t('authentication.login.error.title')}</h2>
+          <p>{ t('authentication.login.error.error_explainer') }</p>
+          <p>{ t('authentication.login.error.error_impact') }</p>
+        </div>
+
+        <section>
+          <h3 className='text-lg font-semibold'>{ t('authentication.login.error.heading_user') }</h3>
+          <p>{t('authentication.login.error.action_user')}</p>
+        </section>
+
+        <section className="pt-8">
+          <h3 className='text-lg font-semibold'>{t('authentication.login.error.heading_admin')}</h3>
+          <p>{ t('authentication.login.error.action_admin') }</p>
+        </section>
+      </div>
+    )
+  }
+
+}
 
 export default async function Login() {
   const { t } = await createTranslation();
-  const authMethods = await getAuthMethods();
 
   return (
     <>
@@ -21,12 +69,11 @@ export default async function Login() {
           </div>
 
           <div>
-            <AuthMethodsList authMethods={authMethods} />
-          </div>
-
-          <div className="grid gap-2">
-            <p>{ t('authentication.login.email.section_title') }</p>
-            <Email />
+            <Suspense fallback={(
+              <p>{ t('authentication.login.loading_methods') }</p>
+            )}>
+              <AuthMethods />
+            </Suspense>
           </div>
 
         </div>
